ce905b695968ea4f3d41babbc0b5048c0ba9c3f9
Nunny Reyes <nunnyreyes@gmail.com>
2020-06-28 02:07:28 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,13 +2,21 @@
 
 def turn_count(board)
   counter = 0
+  index = 0
   
-  board.each do |player|
-    
-  counter += 1 
+  while counter < board.length
+  counter += 1
+  index += 1
   
+  if board[index] == "X" || board[index] == "O"
+  
+  puts "this is the counter #{counter}"
+  return counter
+  end
+ end
 end
 
 
-def current_player
+def current_player(board)
+  puts "hello"
 end
